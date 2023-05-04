4617b60d0cead2d39a746ce37cd58037cb5bdc83
Pushpendra8887 <132488793+Pushpendra8887@users.noreply.github.com>
2023-05-04 22:17:03 +0300

    Add files via upload

---
+++ Instance versus Static Members.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Chetu
+{
+    class Program
+    {
+        public string Name;//Instance Field
+        public static int population;// static Field
+        public  Program(string n)
+        {
+            Name = n; //Assign the instance field
+            population = population + 1;//Increment the static population
+        }
+       
+        
+        public static void Main(string[] args)
+        {
+            Program t1 = new Program("Hardik");
+            Console.WriteLine(t1.Name);
+            Console.WriteLine(Program.population);
+             
+        }
+
+
+
+
+
+    }
+    }
+       
+
+
+
