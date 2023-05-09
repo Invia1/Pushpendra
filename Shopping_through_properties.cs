ef2e3c6ef9dc7dfc72fe5e8052985f9d794d666d
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-09 13:42:31 +0300

    Add files via upload

---
+++ Shopping_through_properties.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CheckforShoping
+{
+    class shop
+    {
+        double ammount;
+        public double cusmoteramount
+        {
+            get
+            {
+                return ammount;
+            }
+            set 
+            {
+                ammount = value;
+                if (ammount > 10000)
+                {
+                    Console.WriteLine("You are eligable for shopping ");
+                }
+                else 
+                {
+                    Console.WriteLine("You are not eligable for shopping ");
+                }
+            }
+        }
+
+    }
+    class shopedetails
+    {
+        public void custominfo()
+        {
+            shop obj = new shop();
+            Console.WriteLine("Enter the amount for shopping");
+            obj.cusmoteramount = int.Parse(Console.ReadLine());
+        }
+         
+    }
+    class customer
+    {
+
+        static void Main(string[] args)
+        {
+            var obj = new shopedetails();
+            obj.custominfo();
+        }
+    }
+}
