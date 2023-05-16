def215ed4dbc6ff32ba6cac64cc0bf873ffbb7b0
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-16 16:51:52 +0530

    Add files via upload

---
+++ Crud_with_three_layer/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+//Model
+
+namespace SmallEmployeeApp.Models
+{
+    public class Employee
+    {
+        public int EID { get; set; }
+        public string Ename { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Crud_with_three_layer/UI/MainApp.cs
@@ -0,0 +1,38 @@
+﻿using SmallEmployeeApp.Models;
+using SmallEmployeeApp.Service;
+
+namespace SmallEmployeeApp.UI
+{
+    public class MainApp
+    {
+        public static void Main()
+        {
+            Employee obj = new Employee();
+            Employee obj1 = new Employee();
+            obj.EID = 101;
+            obj.Ename = "Deepti";
+            obj.Age = 21;
+
+            obj1.EID = 102;
+            obj1.Ename = "Deepak";
+            obj1.Age = 24;
+
+            EmployeeService es = new EmployeeService();
+            es.CreateEmployee(obj);
+            es.CreateEmployee(obj1);
+            //Console.WriteLine("Enter the u_id ");
+            //int u_id = int.Parse(Console.ReadLine());
+
+            //es.UpdateEmployee( 102);
+            es.search(102);
+
+
+            List<Employee> objList = es.GetEmployeeList();
+            foreach (var data in objList)
+            {
+                Console.WriteLine(data.EID + " " + data.Ename + " " + data.Age);
+            }
+        }
+
+    }
+}
